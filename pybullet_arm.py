25a0258571edeb5e611aab74f147d515615da099
dufrenekm <dufrenekm@gmail.com>
2024-03-17 09:41:48 -0700

    added collisions

--- pybullet_arm.py
+++ pybullet_arm.py
@@ -5,17 +5,27 @@ from time import sleep
 import math
 class PyBulletIK:
     def __init__(self) -> None:
-        physicsClient = p.connect(p.DIRECT)#or p.DIRECT for non-graphical version or GUI
+        physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version or GUI
+        # Define collision detection parameters for the robot arm
+        collisionFilterGroup = 0
+        collisionFilterMask = 1
+
+        
+
         p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-        planeId = p.loadURDF("plane.urdf")
+        self.planeId = p.loadURDF("plane.urdf", physicsClientId=physicsClient)
         p.setAdditionalSearchPath('/home/roosh/Intro2ArmPi/Functions/py_bullet_arm_ik') #optionally
         p.setGravity(0,0,-10)
         startPos = [0,0,-0.08]
         startOrientation = p.getQuaternionFromEuler([0,0,0])
-        self.pi_arm_ID = p.loadURDF("pi_arm.urdf", startPos, startOrientation)
+        self.pi_arm_ID = p.loadURDF("pi_arm.urdf", startPos, startOrientation, physicsClientId=physicsClient)
+        self.scene = p.loadURDF("camera_mount.urdf", [0,0,0], p.getQuaternionFromEuler([0,0, 1.57079632679]))
         self.ee = 5
         self.numJoints = 5
         self.threshold = 0.01
+        # Enable collision detection for the robot arm with the ground
+        for link_index in range(p.getNumJoints(self.pi_arm_ID)):
+            p.setCollisionFilterGroupMask(self.pi_arm_ID, link_index, collisionFilterGroup, collisionFilterMask)
     
     def move_arm(self, target = [0,0,0,0,0]):
         """Reloads arm in final position - no collisions calculated"""
@@ -71,6 +81,49 @@ class PyBulletIK:
         if self.threshold < dist2:
             return False
         return jointPoses
+    
+    def check_trajectory(self, current_angles, target_angles):
+        """ Checks if a final pose is valid (no collisions) with sim arm. 
+        True = valid
+        False = collision
+        """
+
+        # Set arm to current position
+        for i in range(5):
+            p.resetJointState(self.pi_arm_ID, i+1, current_angles[i])
+        # Set up joint controller
+        for i in range(5):
+            p.setJointMotorControl2(bodyIndex=self.pi_arm_ID,
+                                    jointIndex=i+1,
+                                    controlMode=p.POSITION_CONTROL,
+                                    targetPosition=target_angles[i],
+                                    targetVelocity=0,
+                                    force=500,
+                                    positionGain=0.03,
+                                    velocityGain=1)
+        # Returns False if there are collisions
+        done = False
+        while not done:
+            p.stepSimulation()
+            # Check if at target
+            done = self.check_at_target(p.getJointStates(self.pi_arm_ID, [1,2,3,4,5]), target_angles)
+
+            collisions = p.getContactPoints(bodyA=self.pi_arm_ID, bodyB=self.scene)
+            if collisions:
+                return False
+        return True
+
+
+
+    
+    def check_at_target(self, current_angles, target_angles):
+        all_done = True
+        for i, target in enumerate(target_angles):
+            diff = target - current_angles[i][0]
+            
+            if abs(diff)>.01:
+                all_done = False
+        return all_done
         
 if __name__ == '__main__':
     IK = PyBulletIK()
--- test.py
+++ test.py
@@ -1,23 +1,13 @@
 from pybullet_arm import PyBulletIK
 from time import sleep
 import pybullet as p
+
 IK = PyBulletIK()
-# for i in range(100):
-jointPoses = IK.compute_ik([.1, 0, .1])
 
-for i in [0,1,2,3,4,5]:
-        p.setJointMotorControl2(bodyIndex=IK.pi_arm_ID,
-                                jointIndex=i,
-                                controlMode=p.POSITION_CONTROL,
-                                targetPosition=jointPoses[i],
-                                targetVelocity=0,
-                                force=500,
-                                positionGain=0.03,
-                                velocityGain=1)
+jointPoses = [IK.compute_ik([.15,0, .2]),IK.compute_ik([.15, 0, -.05]),IK.compute_ik([.15,0, .2])] #IK.compute_ik([.1, .1, .1]), IK.compute_ik([.1, 0, .1]), IK.compute_ik([.1, .1, .1]), IK.compute_ik([0,0, .25])]#, IK.compute_ik([.15,0, -.05])]
 
+angles = p.getJointStates(IK.pi_arm_ID, [1,2,3,4,5])
+current = [angles[0][0], angles[1][0], angles[2][0], angles[3][0], angles[4][0]]
 
+print(IK.check_trajectory(current, jointPoses[1]))
 
-
-while True:
-    p.stepSimulation()
-    sleep(1)
