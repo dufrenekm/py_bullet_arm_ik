50084e018d9b410b9dbb6693cc961e0cece5f0d2
dufrenekm <dufrenekm@gmail.com>
2024-03-17 09:42:51 -0700

    Checked time

--- test.py
+++ test.py
@@ -9,5 +9,12 @@ jointPoses = [IK.compute_ik([.15,0, .2]),IK.compute_ik([.15, 0, -.05]),IK.comput
 angles = p.getJointStates(IK.pi_arm_ID, [1,2,3,4,5])
 current = [angles[0][0], angles[1][0], angles[2][0], angles[3][0], angles[4][0]]
 
+import time
+
+start = time.time()
+
 print(IK.check_trajectory(current, jointPoses[1]))
+print("hello")
+end = time.time()
+print(end - start)
 
